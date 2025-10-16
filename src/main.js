655121809718afa5e1813d2b1a0e99c706be358d
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2025-10-17 02:15:30 +0300

    init: init Seo-project

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./style/main.css";
+
+// Импортируем JS модули
+import "./js/burger-menu.js";
+import "./js/timer.js";
+import "./js/form-validation.js";
+
+console.log("🚀 SEO System project started!");
